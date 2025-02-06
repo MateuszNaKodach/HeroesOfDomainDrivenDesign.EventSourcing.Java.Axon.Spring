1a6c16a9342773a5867edd56d5d9f63c33fbf346
Mateusz Nowak <mateusz@nakodach.pl>
2025-02-06 23:16:02 +0100

    test works

--- src/test/java/com/dddheroes/heroesofddd/creaturerecruitment/read/getdwellingbyid/GetDwellingByIdTest.java
+++ src/test/java/com/dddheroes/heroesofddd/creaturerecruitment/read/getdwellingbyid/GetDwellingByIdTest.java
@@ -3,13 +3,20 @@ package com.dddheroes.heroesofddd.creaturerecruitment.read.getdwellingbyid;
 import com.dddheroes.heroesofddd.TestcontainersConfiguration;
 import com.dddheroes.heroesofddd.creaturerecruitment.read.DwellingReadModel;
 import com.dddheroes.heroesofddd.creaturerecruitment.write.builddwelling.DwellingBuilt;
+import org.awaitility.Awaitility;
+import org.awaitility.core.AssertionCondition;
+import org.awaitility.core.Condition;
+import org.awaitility.core.ThrowingRunnable;
+import org.axonframework.eventhandling.GenericDomainEventMessage;
 import org.axonframework.eventhandling.gateway.EventGateway;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.queryhandling.QueryGateway;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+import java.time.Duration;
 import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.*;
@@ -24,6 +31,9 @@ class GetDwellingByIdTest {
     @Autowired
     private EventGateway eventGateway;
 
+    @Autowired
+    private EventStore eventStore;
+
     @Test
     void dwellingNotExist() {
         // when
@@ -36,12 +46,37 @@ class GetDwellingByIdTest {
     @Test
     void dwellingExist() {
         // given
-        eventGateway.publish(new DwellingBuilt("2", "2", new HashMap<>()));
+//        eventGateway.publish(new DwellingBuilt("2", "2", new HashMap<>()));
+        eventStore.publish(new GenericDomainEventMessage<>(
+                "Dwelling",
+                "2",
+                0,
+                new DwellingBuilt("2", "2", new HashMap<>())
+        ));
 
         // when
-        var result = queryGateway.query(GetDwellingById.query("2"), DwellingReadModel.class).join();
+        var query = GetDwellingById.query("2");
 
         // then
-        assertThat(result).isNotNull();
+        awaitUntilAsserted(() -> {
+            var result = queryGateway.query(GetDwellingById.query("2"), DwellingReadModel.class).join();
+            assertThat(result).isNotNull();
+        });
+    }
+
+    private void dwellingDomainEvent(String dwellingId, int sequenceNumber, Object payload) {
+        eventGateway.publish(new GenericDomainEventMessage<>(
+                "Dwelling",
+                dwellingId,
+                sequenceNumber,
+                new DwellingBuilt(dwellingId, dwellingId, new HashMap<>())
+        ));
+    }
+
+    private void awaitUntilAsserted(Runnable assertion) {
+        Awaitility.await()
+                  .pollInSameThread()
+                  .atMost(Duration.ofSeconds(5))
+                  .untilAsserted(assertion::run);
     }
 }
