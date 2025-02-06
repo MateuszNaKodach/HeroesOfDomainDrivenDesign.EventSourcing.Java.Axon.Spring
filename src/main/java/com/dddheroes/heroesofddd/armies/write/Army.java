78775e3c21dd3a5af9aedef8e627eb55b138bd2f
Mateusz Nowak <mateusz@nakodach.pl>
2025-02-06 11:50:55 +0100

    remove creature from army

---
+++ src/main/java/com/dddheroes/heroesofddd/armies/write/Army.java
@@ -0,0 +1,5 @@
+package com.dddheroes.heroesofddd.armies.write;
+
+public class Army {
+
+}
---
+++ src/main/java/com/dddheroes/heroesofddd/armies/write/removecreature/CreatureRemovedFromArmy.java
@@ -0,0 +1,25 @@
+package com.dddheroes.heroesofddd.armies.write.removecreature;
+
+import com.dddheroes.heroesofddd.armies.write.ArmyEvent;
+import com.dddheroes.heroesofddd.shared.Amount;
+import com.dddheroes.heroesofddd.shared.ArmyId;
+import com.dddheroes.heroesofddd.shared.CreatureId;
+
+public record CreatureRemovedFromArmy(
+        String armyId,
+        String creatureId,
+        Integer quantity
+) implements ArmyEvent {
+
+    public static CreatureRemovedFromArmy event(
+            ArmyId armyId,
+            CreatureId creatureId,
+            Amount quantity
+    ) {
+        return new CreatureRemovedFromArmy(
+                armyId.raw(),
+                creatureId.raw(),
+                quantity.raw()
+        );
+    }
+}
