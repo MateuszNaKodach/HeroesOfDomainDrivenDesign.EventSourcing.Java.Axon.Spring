31ba89815163126881afea0e2d0154ffee38abbf
Mateusz Nowak <mateusz@nakodach.pl>
2025-02-08 00:55:21 +0100

    StartDayTest

--- src/test/java/com/dddheroes/heroesofddd/calendar/write/startday/StartDayTest.java
+++ src/test/java/com/dddheroes/heroesofddd/calendar/write/startday/StartDayTest.java
@@ -5,7 +5,11 @@ import com.dddheroes.heroesofddd.calendar.write.CalendarTest;
 import com.dddheroes.heroesofddd.calendar.write.Day;
 import com.dddheroes.heroesofddd.calendar.write.Month;
 import com.dddheroes.heroesofddd.calendar.write.Week;
+import com.dddheroes.heroesofddd.calendar.write.finishday.DayFinished;
+import com.dddheroes.heroesofddd.shared.DomainRule;
 import org.junit.jupiter.api.*;
+
+import java.util.List;
 
 public class StartDayTest extends CalendarTest {
 
@@ -22,6 +26,44 @@ public class StartDayTest extends CalendarTest {
         fixture.givenNoPriorActivity()
                 .when(whenCommand)
                 .expectEvents(thenEvent);
+    }
+
+    @Test
+    void givenPreviousDayFinished_WhenStartNextDay_ThenSuccess() {
+        // given
+        var calendarId = CalendarId.random();
+        var givenEvents = List.of(
+                DayStarted.event(calendarId, Month.of(1), Week.of(1), Day.of(1)),
+                DayFinished.event(calendarId, Month.of(1), Week.of(1), Day.of(1))
+        );
+
+        // when
+        var whenCommand = StartDay.command(calendarId.raw(), 1, 1, 2);
+
+        // then
+        var thenEvent = DayStarted.event(calendarId, Month.of(1), Week.of(1), Day.of(2));
+        fixture.given(givenEvents)
+               .when(whenCommand)
+               .expectEvents(thenEvent);
+    }
+
+    @Test
+    void givenPreviousDayFinished_WhenStartDaySkipping_ThenException() {
+        // given
+        var calendarId = CalendarId.random();
+        var givenEvents = List.of(
+                DayStarted.event(calendarId, Month.of(1), Week.of(1), Day.of(1)),
+                DayFinished.event(calendarId, Month.of(1), Week.of(1), Day.of(1))
+        );
+
+        // when
+        var whenCommand = StartDay.command(calendarId.raw(), 1, 1, 3);
+
+        // then
+        fixture.given(givenEvents)
+               .when(whenCommand)
+               .expectException(DomainRule.ViolatedException.class)
+               .expectExceptionMessage("Cannot skip days");
     }
 
 }
