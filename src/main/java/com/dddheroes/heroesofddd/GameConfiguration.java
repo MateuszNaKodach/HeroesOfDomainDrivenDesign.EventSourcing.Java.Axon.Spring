f7751a641750adde97edfa015117c41ad1db8cbf
Mateusz Nowak <mateusz@nakodach.pl>
2025-03-05 11:55:40 +0100

    :sparkles: feat: `CorrelationDataProvider` with `gameId`, `playerId` and the Axon defaults

--- src/main/java/com/dddheroes/heroesofddd/GameConfiguration.java
+++ src/main/java/com/dddheroes/heroesofddd/GameConfiguration.java
@@ -3,6 +3,9 @@ package com.dddheroes.heroesofddd;
 import com.dddheroes.heroesofddd.shared.application.GameMetaData;
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.eventhandling.async.SequencingPolicy;
+import org.axonframework.messaging.correlation.CorrelationDataProvider;
+import org.axonframework.messaging.correlation.MessageOriginProvider;
+import org.axonframework.messaging.correlation.SimpleCorrelationDataProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +15,15 @@ public class GameConfiguration {
     @Bean
     public SequencingPolicy<EventMessage<?>> gameIdSequencingPolicy() {
         return e -> e.getMetaData().get(GameMetaData.GAME_ID_KEY);
+    }
+
+    @Bean
+    public CorrelationDataProvider gameDataProvider() {
+        return new SimpleCorrelationDataProvider(GameMetaData.GAME_ID_KEY, GameMetaData.PLAYER_ID_KEY);
+    }
+
+    @Bean
+    public CorrelationDataProvider messageOriginProvider() {
+        return new MessageOriginProvider();
     }
 }
