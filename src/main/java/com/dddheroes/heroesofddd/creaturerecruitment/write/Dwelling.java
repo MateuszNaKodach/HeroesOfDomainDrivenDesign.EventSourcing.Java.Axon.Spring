3a771f5a674e874958e2217abb4e6e8d95561649
Mateusz Nowak <mateusz@nakodach.pl>
2025-07-08 14:14:15 +0200

    snapshot filter misused

--- src/main/java/com/dddheroes/heroesofddd/creaturerecruitment/write/Dwelling.java
+++ src/main/java/com/dddheroes/heroesofddd/creaturerecruitment/write/Dwelling.java
@@ -24,7 +24,7 @@ import org.slf4j.LoggerFactory;
 
 import static org.axonframework.modelling.command.AggregateLifecycle.*;
 
-@Aggregate(snapshotTriggerDefinition = "dwellingSnapshotTrigger", snapshotFilter = "dwellingSnapshotFilter")
+@Aggregate(snapshotTriggerDefinition = "dwellingSnapshotTrigger")
 public class Dwelling {
 
     private static final Logger logger = LoggerFactory.getLogger(Dwelling.class);
