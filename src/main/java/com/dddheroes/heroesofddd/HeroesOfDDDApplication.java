dc462a463e6e2e911184a077bd71cbc776e011fc
Mateusz Nowak <mateusz@nakodach.pl>
2026-05-20 23:53:20 +0200

    refactor(af5): `EventStoreConfiguration` - not needed

--- src/main/java/com/dddheroes/heroesofddd/HeroesOfDDDApplication.java
+++ src/main/java/com/dddheroes/heroesofddd/HeroesOfDDDApplication.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {
-		"com.dddheroes.heroesofddd",
-		"org.axonframework",
-		"io.axoniq.framework"
-})
 public class HeroesOfDDDApplication {
 
 	public static void main(String[] args) {
